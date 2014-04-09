3e2c80c68e1f78d6185550339adf829747d3e192
U-ccm\Administrator <Administrator@xzg-notebook.(none)>
2014-04-10 00:54:42 +0800

    ss

---
+++ exobudpl.js
@@ -0,0 +1,5 @@
+mkList("http://219.223.195.113/music/1.mp3","1.mp3");
+mkList("http://219.223.195.113/music/2.mp3","2.mp3");
+mkList("http://219.223.195.113/music/3.mp3","3.mp3");
+mkList("http://219.223.195.113/music/4.mp3","4.mp3");
+mkList("http://219.223.195.113/music/5.mp3","5.mp3");
